96b9176be5b4ad5c4561abfdb0e89e910a7ccae2
Kolchi <kolchiibrahim@icloud.com>
2026-03-05 00:53:53 +0400

    Update LibraryCoordinator.swift

--- EchoShelf/Library/LibraryCoordinator.swift
+++ EchoShelf/Library/LibraryCoordinator.swift
@@ -5,4 +5,46 @@
 //  Created by Ibrahim Kolchi on 05.03.26.
 //
 
-import Foundation
+import UIKit
+
+final class LibraryCoordinator: Coordinator {
+
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vm = LibraryViewModel()
+        let vc = LibraryViewController(viewModel: vm)
+        vc.coordinator = self
+        navigationController.setViewControllers([vc], animated: false)
+    }
+
+    // MARK: - Navigation
+
+    func openBook(_ item: LibraryItem) {
+        guard let url = item.localURL else { return }
+
+        // EbookReaderViewController-i local URL ilə aç
+        let vc = LibraryReaderViewController(item: item, localURL: url)
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: true)
+    }
+
+    func confirmDelete(_ item: LibraryItem,
+                       from vc: UIViewController,
+                       onConfirm: @escaping () -> Void) {
+        let alert = UIAlertController(
+            title: "Remove from Library?",
+            message: "\"\(item.title)\" will be deleted from your device.",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Remove", style: .destructive) { _ in
+            onConfirm()
+        })
+        vc.present(alert, animated: true)
+    }
+}
