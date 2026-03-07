7fc8c4065b89f512350e7bd88f492aae4052c928
Kolchi <kolchiibrahim@icloud.com>
2026-03-07 13:39:36 +0400

    Lottie

--- EchoShelf/App/AppDelegate.swift
+++ EchoShelf/App/AppDelegate.swift
@@ -9,14 +9,11 @@ import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        
         FirebaseApp.configure()
-        
         return true
     }
 
