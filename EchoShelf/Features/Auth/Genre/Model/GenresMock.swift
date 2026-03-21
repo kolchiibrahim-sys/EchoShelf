130e043979ce3013f6354c5f4111d14ea6c98e3b
Kolchi <kolchiibrahim@icloud.com>
2026-03-22 00:14:19 +0400

    Genres Mock Model

---
+++ EchoShelf/Features/Auth/Genre/Model/GenresMock.swift
@@ -0,0 +1,14 @@
+//
+//  GenresMock.swift
+//  EchoShelf
+//
+//  Created by Ibrahim Kolchi on 22.03.26.
+//
+import Foundation
+
+struct Genre: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
